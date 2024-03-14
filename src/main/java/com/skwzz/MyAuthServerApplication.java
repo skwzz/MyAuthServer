24188cb6389879a090981166a0ecdfb5b3d351a7
skwzz <kidol9108@gmail.com>
2024-03-14 23:54:09 +0900

    First Commit

---
+++ src/main/java/com/skwzz/MyAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.skwzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyAuthServerApplication.class, args);
+    }
+
+}
