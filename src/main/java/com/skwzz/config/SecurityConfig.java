ca2100129ae13a499453d4da0bf56d2388e9e16f
skwzz <kidol9108@gmail.com>
2024-03-16 15:28:09 +0900

    회원가입 비밀번호에 passwordencoder 적용

--- src/main/java/com/skwzz/config/SecurityConfig.java
+++ src/main/java/com/skwzz/config/SecurityConfig.java
@@ -1,10 +1,13 @@
 package com.skwzz.config;
 
 import com.skwzz.global.exception.CustomAccessDeniedHandler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -20,5 +23,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
                 .authorizeRequests()
                 .antMatchers("/api/auth/**").permitAll()
                 .anyRequest().authenticated();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/skwzz/domain/member/service/MemberService.java
+++ src/main/java/com/skwzz/domain/member/service/MemberService.java
@@ -1,9 +1,11 @@
 package com.skwzz.domain.member.service;
 
 import com.skwzz.domain.auth.payload.request.SignUpDto;
+import com.skwzz.domain.member.entity.Member;
 import com.skwzz.domain.member.mapper.MemberMapper;
 import com.skwzz.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,9 +14,14 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional
     public void signUp(SignUpDto signUpDto){
-        memberRepository.save(MemberMapper.dtoToEntity(signUpDto));
+        Member member = Member.builder()
+                .username(signUpDto.getUsername())
+                .password(passwordEncoder.encode(signUpDto.getPassword()))
+                .build();
+        memberRepository.save(member);
     }
 }
